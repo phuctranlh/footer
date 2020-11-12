66513ce8b327f0237aa5d07e108a5159402b27f1
phuctranlh <phuctran.lh@gmail.com>
2020-11-12 22:04:09 +1000

    Add subscription session
     - Add html
    - Add style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,27 @@ function Footer(props) {
     return (
 
         <div className={'footer-container'}>
-            
+            <section className={'footer-subscription'}>
+
+                <p className={'footer-subscription-heading'}>
+                    Join our newsletter to receive our best articles
+                </p>
+                <p className={'footer-subscription-text'}>
+                    You can unsubscribe at any time.
+                </p>
+                <div className={'input-area'}>
+                    <form>
+                        <input
+                            className={'footer-input'}
+                            name='email'
+                            type='email'
+                            placeholder='Your Email'
+                        />
+                        <a href="sign-in" className={' btn-outline'}>Subscribe</a>
+                    </form>
+                </div>
+
+            </section>
         </div>
     );
 }
